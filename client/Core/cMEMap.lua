41819e0d84886d365bf6e4d298ae04a3b460f44d
dreadmullet <loosepolygon@gmail.com>
2014-05-02 03:47:10 +0000

    Merged Editor class into the previously underused Map class.

--- client/Core/cMEMap.lua
+++ client/Core/cMEMap.lua
@@ -1,7 +1,49 @@
 class("Map" , MapEditor)
 
-function MapEditor.Map:__init()
+function MapEditor.Map:__init(initialPosition)
+	EGUSM.SubscribeUtility.__init(self)
 	MapEditor.Marshallable.__init(self)
 	MapEditor.ObjectManager.__init(self)
 	MapEditor.PropertyManager.__init(self)
+	
+	self.Destroy = MapEditor.Map.Destroy
+	
+	MapEditor.map = self
+	
+	Controls.Add("Rotate/pan camera" , "VehicleCam")
+	Controls.Add("Camera pan modifier" , "Shift")
+	self.camera = MapEditor.Camera(initialPosition)
+	
+	MapEditor.SpawnMenu()
+	
+	Mouse:SetVisible(true)
+	
+	self:EventSubscribe("ControlDown")
+	self:EventSubscribe("ControlUp")
+end
+
+function MapEditor.Map:Destroy()
+	self:UnsubscribeAll()
+	
+	self.camera:Destroy()
+	
+	self:IterateObjects(function(object)
+		object:Destroy()
+	end)
+	
+	Mouse:SetVisible(false)
+end
+
+-- Events
+
+function MapEditor.Map:ControlDown(args)
+	if args.name == "Rotate/pan camera" then
+		self.camera.isInputEnabled = true
+	end
+end
+
+function MapEditor.Map:ControlUp(args)
+	if args.name == "Rotate/pan camera" then
+		self.camera.isInputEnabled = false
+	end
 end
--- client/cTesting.lua
+++ client/cTesting.lua
@@ -1,7 +1,7 @@
 Events:Subscribe("ModuleLoad" , function()
-	editor = MapEditor.Editor(Vector3(-6550 , 215 , -3290))
+	map = MapEditor.Map(Vector3(-6550 , 215 , -3290))
 end)
 
 Events:Subscribe("ModuleUnload" , function()
-	editor:Destroy()
+	map:Destroy()
 end)
