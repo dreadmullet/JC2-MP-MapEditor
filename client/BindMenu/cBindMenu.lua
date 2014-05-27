200bc84fc68e64c0425b8b5d3eaabf1642f94203
dreadmullet <loosepolygon@gmail.com>
2014-05-27 00:30:27 +0000

    Updated bind menu.

--- client/BindMenu/cBindMenu.lua
+++ client/BindMenu/cBindMenu.lua
@@ -1,7 +1,6 @@
-
 BindMenu = {}
 
--- Controls cannot be assigned to these if allowMouse is false (default).
+-- Controls can only be assigned to these if using a gamepad.
 BindMenu.blockedMouseActions = {
 	Action.LookUp ,
 	Action.LookDown ,
@@ -23,15 +22,22 @@ BindMenu.Create = function(...)
 	window.state = "Idle"
 	window.eventInput = nil
 	window.eventKeyUp = nil
+	window.eventMouseUp = nil
+	window.eventMouseWheel = nil
+	window.eventPostTick = nil
 	window.activatedButton = nil
-	window.allowMouse = false
 	window.receiveEvent = nil
 	window.tickEvent = nil
 	window.dirtySettings = false
 	window.saveTimer = Timer()
+	-- These two are used to delay Actions so it prefers keys or mouse buttons.
+	window.activeAction = nil
+	window.ticksSinceAction = 0
+	-- Used to determine mouse delta.
+	window.mousePositionStart = nil
 	
 	-- defaultControl can be an Action name, a Key name, or nil.
-	-- Examples: "SoundHornSiren", "LShift", "C", nil
+	-- Examples: "SoundHornSiren", "LShift", "C", "Mouse3", nil
 	function window:AddControl(name , defaultControl)
 		if not name then
 			error("Invalid arguments")
@@ -46,29 +52,32 @@ BindMenu.Create = function(...)
 		button:SetDataObject("control" , control)
 		button:Subscribe("Press" , self , self.ButtonPressed)
 		button:Subscribe("RightPress" , self , self.ButtonPressed)
-		table.insert(self.buttons , button)
+		local baseButton = button
+		table.insert(self.buttons , baseButton)
 		
-		local unassignButton = Button.Create(button)
-		unassignButton:SetDock(GwenPosition.Right)
-		unassignButton:SetText(" X ")
-		unassignButton:SizeToContents()
-		unassignButton:SetToolTip("Unassign")
-		unassignButton:SetTextNormalColor(Color(220 , 50 , 50))
-		unassignButton:SetTextPressedColor(Color(150 , 40 , 40))
-		unassignButton:SetTextHoveredColor(Color(255 , 70 , 70))
-		unassignButton:Subscribe("Down" , self , self.UnassignButtonPressed)
+		-- Unassign button
+		local button = Button.Create(baseButton)
+		button:SetDock(GwenPosition.Right)
+		button:SetText(" X ")
+		button:SizeToContents()
+		button:SetToolTip("Unassign")
+		button:SetTextNormalColor(Color(220 , 50 , 50))
+		button:SetTextPressedColor(Color(150 , 40 , 40))
+		button:SetTextHoveredColor(Color(255 , 70 , 70))
+		button:Subscribe("Down" , self , self.UnassignButtonPressed)
 		
-		local labelValue = Label.Create(button)
-		labelValue:SetTextColor(button:GetTextColor())
-		labelValue:SetAlignment(GwenPosition.Right)
-		labelValue:SetDock(GwenPosition.Right)
-		labelValue:SetPadding(Vector2(4 , 4) , Vector2(4 , 4))
-		labelValue:SetText(control.valueString)
-		labelValue:SizeToContents()
+		-- Value label
+		local label = Label.Create(baseButton)
+		label:SetTextColor(baseButton:GetTextColor())
+		label:SetAlignment(GwenPosition.Right)
+		label:SetDock(GwenPosition.Right)
+		label:SetPadding(Vector2(4 , 4) , Vector2(4 , 4))
+		label:SetText(control.valueString)
+		label:SizeToContents()
 		
-		button:SetDataObject("label" , labelValue)
+		baseButton:SetDataObject("label" , label)
 		
-		self:Assign(button)
+		self:Assign(baseButton)
 	end
 	
 	function window:Assign(activeButton)
@@ -91,6 +100,8 @@ BindMenu.Create = function(...)
 		if IsValid(self.eventInput) then
 			Events:Unsubscribe(self.eventInput)
 			Events:Unsubscribe(self.eventKeyUp)
+			Events:Unsubscribe(self.eventMouseUp)
+			Events:Unsubscribe(self.eventMouseWheel)
 		end
 	end
 	
@@ -109,9 +120,30 @@ BindMenu.Create = function(...)
 		label:SetText("...")
 		
 		self.activatedButton = button
+		self.activeAction = nil
+		self.mousePositionStart = Mouse:GetPosition()
 		
 		self.eventInput = Events:Subscribe("LocalPlayerInput" , self , self.LocalPlayerInput)
 		self.eventKeyUp = Events:Subscribe("KeyUp" , self , self.KeyUp)
+		self.eventMouseUp = Events:Subscribe("MouseUp" , self , self.MouseButtonUp)
+		self.eventMouseWheel = Events:Subscribe("MouseScroll" , self , self.MouseScroll)
+	end
+	
+	function window:MouseMovementButtonPressed(button)
+		if self.state ~= "Idle" then
+			return
+		end
+		
+		self.state = "ActivatedMouse"
+		
+		BindMenu.SetEnabledRecursive(self , false)
+		
+		self.activatedButton = button:GetParent()
+		
+		self.mousePositionStart = Mouse:GetPosition()
+		
+		local label = self.activatedButton:GetDataObject("label")
+		label:SetText("...")
 	end
 	
 	function window:UnassignButtonPressed(button)
@@ -136,17 +168,17 @@ BindMenu.Create = function(...)
 	end
 	
 	function window:SaveSettings()
-		local settings = ""
+		local settings = {}
 		
 		-- Marshal every control into a format that will be stored in the database.
 		for index , control in ipairs(Controls.controls) do
-			local type = "0"
-			if control.type == "Action" then
-				type = "1"
-			elseif control.type == "Key" then
-				type = "2"
-			end
-			settings = settings..control.name.."|"..type.."|"..tostring(control.value).."\n"
+			local info = {
+				name = control.name ,
+				type = control.type ,
+				value = tostring(control.value) ,
+				module = module_name ,
+			}
+			table.insert(settings , info)
 		end
 		
 		Network:Send("BindMenuSaveSettings" , settings)
@@ -163,7 +195,8 @@ BindMenu.Create = function(...)
 	-- Events
 	
 	function window:LocalPlayerInput(args)
-		if self.allowMouse == false then
+		-- Block mouse actions if we're not using a gamepad.
+		if Game:GetSetting(GameSetting.GamepadInUse) == 0 then
 			for index , action in ipairs(BindMenu.blockedMouseActions) do
 				if args.input == action then
 					return true
@@ -171,16 +204,11 @@ BindMenu.Create = function(...)
 			end
 		end
 		
-		local control = self.activatedButton:GetDataObject("control")
-		
-		control.type = "Action"
-		control.value = args.input
-		control.valueString = InputNames.GetActionName(args.input)
-		
-		self:Assign(self.activatedButton)
-		self.activatedButton = nil
-		
-		self.dirtySettings = true
+		if args.state > 0.5 then
+			-- PostTick handles these later.
+			self.activeAction = args.input
+			self.ticksSinceAction = 0
+		end
 		
 		return true
 	end
@@ -189,11 +217,37 @@ BindMenu.Create = function(...)
 		local control = self.activatedButton:GetDataObject("control")
 		
 		control.type = "Key"
-		control.value = args.key		
+		control.value = args.key
 		control.valueString = InputNames.GetKeyName(args.key)
 		
-		if control.valueString == "INVALID" then
-			control.valueString = string.char(args.key) or "Unknown"
+		self:Assign(self.activatedButton)
+		self.activatedButton = nil
+		
+		self.dirtySettings = true
+	end
+	
+	function window:MouseButtonUp(args)
+		local control = self.activatedButton:GetDataObject("control")
+		
+		control.type = "MouseButton"
+		control.value = args.button
+		control.valueString = string.format("Mouse%i" , args.button)
+		
+		self:Assign(self.activatedButton)
+		self.activatedButton = nil
+		
+		self.dirtySettings = true
+	end
+	
+	function window:MouseScroll(args)
+		local control = self.activatedButton:GetDataObject("control")
+		
+		control.type = "MouseWheel"
+		control.value = math.clamp(args.delta , -1 , 1)
+		if control.value == 1 then
+			control.valueString = "Mouse wheel up"
+		else
+			control.valueString = "Mouse wheel down"
 		end
 		
 		self:Assign(self.activatedButton)
@@ -203,7 +257,56 @@ BindMenu.Create = function(...)
 	end
 	
 	function window:PostTick()
-		if self.saveTimer:GetSeconds() > 9 then
+		if self.state == "Activated" then
+			-- If we've tried to assign an action a few ticks ago, actually assign it. Actions are
+			-- delayed so that keys and mouse buttons have preference.
+			if self.activeAction then
+				self.ticksSinceAction = self.ticksSinceAction + 1
+				if self.ticksSinceAction >= 3 then
+					local control = self.activatedButton:GetDataObject("control")
+					control.type = "Action"
+					control.value = self.activeAction
+					control.valueString = InputNames.GetActionName(self.activeAction)
+					
+					self:Assign(self.activatedButton)
+					self.activatedButton = nil
+					
+					self.dirtySettings = true
+					
+					self.activeAction = nil
+				end
+			-- Check for mouse movement.
+			else
+				local delta = Mouse:GetPosition() - self.mousePositionStart
+				local requiredDistance = 12
+				
+				local Assign = function(value , valueString)
+					local control = self.activatedButton:GetDataObject("control")
+					
+					control.type = "MouseMovement"
+					control.value = value
+					control.valueString = valueString
+					
+					self:Assign(self.activatedButton)
+					self.activatedButton = nil
+					
+					self.dirtySettings = true
+				end
+				
+				if delta.x > requiredDistance then
+					Assign(">" , "Mouse right")
+				elseif delta.x < -requiredDistance then
+					Assign("<" , "Mouse left")
+				elseif delta.y > requiredDistance then
+					Assign("v" , "Mouse down")
+				elseif delta.y < -requiredDistance then
+					Assign("^" , "Mouse up")
+				end
+			end
+		end
+		
+		-- Give the server our settings periodically.
+		if self.saveTimer:GetSeconds() > 7 then
 			self.saveTimer:Restart()
 			
 			if self.dirtySettings then
@@ -213,32 +316,45 @@ BindMenu.Create = function(...)
 		end
 	end
 	
-	function window:ReceiveSettings(settings)
-		if settings == "Empty" then
+	function window:ReceiveSettings(settingsString)
+		if settingsString == "Empty" then
 			return
 		end
 		
-		local settingsTable = settings:split("\n")
-		for index , setting in ipairs(settingsTable) do
-			if setting:len() < 4 then
-				goto continue
-			end
-			
+		local settings = settingsString:split("\n")
+		for index , setting in ipairs(settings) do
 			local control = {}
-			local name , type , value = table.unpack(setting:split("|"))
+			local module , name , type , value = table.unpack(setting:split("|"))
 			control.name = name
-			if type == "0" then
-				control.type = "Unassigned"
-				control.value = -1
+			control.type = type
+			if control.type == "Unassigned" then
 				control.valueString = "Unassigned"
-			elseif type == "1" then
-				control.type = "Action"
+			elseif control.type == "Action" then
 				control.value = tonumber(value) or -1
 				control.valueString = InputNames.GetActionName(control.value)
-			elseif type == "2" then
-				control.type = "Key"
+			elseif control.type == "Key" then
 				control.value = tonumber(value) or -1
 				control.valueString = InputNames.GetKeyName(control.value)
+			elseif control.type == "MouseButton" then
+				control.value = tonumber(value) or -1
+				control.valueString = ("Mouse%i"):format(value)
+			elseif control.type == "MouseWheel" then
+				control.value = tonumber(value) or 0
+				if control.value == 1 then
+					control.valueString = "Mouse wheel up"
+				elseif control.value == -1 then
+					control.valueString = "Mouse wheel down"
+				else
+					control.valueString = "Mouse wheel wat"
+				end
+			elseif control.type == "MouseMovement" then
+				control.value = value
+				control.valueString = ({
+					[">"] = "Mouse right" ,
+					["<"] = "Mouse left" ,
+					["v"] = "Mouse down" ,
+					["^"] = "Mouse up" ,
+				})[control.value]
 			end
 			
 			for index , button in ipairs(self.buttons) do
@@ -249,8 +365,6 @@ BindMenu.Create = function(...)
 					break
 				end
 			end
-			
-			::continue::
 		end
 	end
 	
