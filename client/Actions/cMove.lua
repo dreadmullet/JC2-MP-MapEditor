a2e4e8c5e12fd160df0d2ef21ef7d42937866c7b
dreadmullet <loosepolygon@gmail.com>
2014-06-05 03:10:02 +0000

    Added isLocal functionality to Move action.

--- client/Actions/cMove.lua
+++ client/Actions/cMove.lua
@@ -13,15 +13,21 @@ function Actions.Move:OnProcess(objectInfo , mouse , pivot)
 	local mult = distance * self.sensitivity
 	local delta = Camera:GetAngle() * Vector3(mouse.delta.x , -mouse.delta.y , 0) * mult
 	
-	if self.lockedAxis == "X" then
-		delta.y = 0
-		delta.z = 0
-	elseif self.lockedAxis == "Y" then
-		delta.x = 0
-		delta.z = 0
-	elseif self.lockedAxis == "Z" then
-		delta.x = 0
-		delta.y = 0
+	if self.lockedAxis then
+		if self.lockedAxis == "X" then
+			delta.y = 0
+			delta.z = 0
+		elseif self.lockedAxis == "Y" then
+			delta.x = 0
+			delta.z = 0
+		elseif self.lockedAxis == "Z" then
+			delta.x = 0
+			delta.y = 0
+		end
+		
+		if self.isLocal then
+			delta = objectInfo.startTransform.angle * delta
+		end
 	end
 	
 	objectInfo.endTransform.position = objectInfo.startTransform.position + delta
