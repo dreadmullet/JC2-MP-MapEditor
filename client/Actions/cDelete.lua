a051cb882a96c3ddae1dac24dc2e4e6a224e66be
dreadmullet <loosepolygon@gmail.com>
2014-06-05 23:27:00 +0000

    Delete now resets any properties that contain the deleted Objects.

--- client/Actions/cDelete.lua
+++ client/Actions/cDelete.lua
@@ -4,8 +4,50 @@ function Actions.Delete:__init()
 	MapEditor.Action.__init(self)
 	
 	self.objects = {}
+	-- If we delete an Object that is referenced elsewhere, those properties get reset.
+	-- Each element is like, {property = Property , originalValue = blar , index = optional}
+	self.properties = {}
+	
 	MapEditor.map.selectedObjects:IterateObjects(function(object)
 		table.insert(self.objects , object)
+	end)
+	
+	local TestProperty = function(property)
+		local IsInObjects = function(propertyValue)
+			if propertyValue ~= MapEditor.Property.NoObject then
+				local objectId = propertyValue:GetId()
+				for index , object in ipairs(self.objects) do
+					if object:GetId() == objectId then
+						return true
+					end
+				end
+			end
+			
+			return false
+		end
+		
+		if Objects[property.type] then
+			if IsInObjects(property.value) then
+				table.insert(self.properties , {property = property , originalValue = property.value})
+			end
+		elseif Objects[property.subtype] then
+			for index , object in ipairs(property.value) do
+				if IsInObjects(object) then
+					local propertyInfo = {
+						property = property ,
+						index = index ,
+						originalValue = object ,
+					}
+					table.insert(self.properties , propertyInfo)
+				end
+			end
+		end
+	end
+	
+	MapEditor.map:IterateProperties(TestProperty)
+	
+	MapEditor.map:IterateObjects(function(object)
+		object:IterateProperties(TestProperty)
 	end)
 	
 	if #self.objects > 0 then
@@ -23,6 +65,14 @@ function Actions.Delete:Undo()
 		MapEditor.map.selectedObjects:AddObject(object)
 	end
 	
+	for index , propertyInfo in ipairs(self.properties) do
+		if propertyInfo.index then
+			propertyInfo.property.value[propertyInfo.index] = propertyInfo.originalValue
+		else
+			propertyInfo.property.value = propertyInfo.originalValue
+		end
+	end
+	
 	MapEditor.map:SelectionChanged()
 end
 
@@ -31,6 +81,14 @@ function Actions.Delete:Redo()
 		object:Destroy()
 		MapEditor.map.selectedObjects:RemoveObject(object)
 		MapEditor.map:RemoveObject(object)
+	end
+	
+	for index , propertyInfo in ipairs(self.properties) do
+		if propertyInfo.index then
+			propertyInfo.property.value[propertyInfo.index] = MapEditor.Property.NoObject
+		else
+			propertyInfo.property.value = MapEditor.Property.NoObject
+		end
 	end
 	
 	MapEditor.map:SelectionChanged()
