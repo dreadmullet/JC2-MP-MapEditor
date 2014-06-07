523600bffc1a4906d5553e0327ab1ba423652eee
dreadmullet <loosepolygon@gmail.com>
2014-06-07 06:24:04 +0000

    Added shift mode to the Move action, which moves the object to the mouse cursor using a raycast.

--- client/Actions/cMove.lua
+++ client/Actions/cMove.lua
@@ -16,48 +16,60 @@ function Actions.Move:__init()
 end
 
 function Actions.Move:OnProcess(objectInfo , mouse , pivot)
-	local distance = Vector3.Distance(Camera:GetPosition() , pivot)
-	local mult = distance * self.sensitivity
-	local delta = Camera:GetAngle() * Vector3(mouse.delta.x , -mouse.delta.y , 0) * mult
-	
-	if self.lockedAxis then
-		if self.lockedAxis == "X" then
-			delta.y = 0
-			delta.z = 0
-		elseif self.lockedAxis == "Y" then
-			delta.x = 0
-			delta.z = 0
-		elseif self.lockedAxis == "Z" then
-			delta.x = 0
-			delta.y = 0
-		end
+	local delta
+	-- Holding shift moves the object to the mouse cursor using a raycast.
+	if Key:IsDown(VirtualKey.Shift) then
+		local result = Physics:Raycast(
+			Camera:GetPosition() ,
+			Render:ScreenToWorldDirection(Mouse:GetPosition()) ,
+			0.2 ,
+			2000
+		)
+		delta = result.position - objectInfo.startTransform.position
+	else
+		local distance = Vector3.Distance(Camera:GetPosition() , pivot)
+		local mult = distance * self.sensitivity
+		delta = Camera:GetAngle() * Vector3(mouse.delta.x , -mouse.delta.y , 0) * mult
 		
-		if self.isLocal then
-			delta = objectInfo.startTransform.angle * delta
-		end
-		
-		if self.gizmoModel then
-			local angle
+		if self.lockedAxis then
 			if self.lockedAxis == "X" then
-				angle = Angle(0 , 0 , 0)
+				delta.y = 0
+				delta.z = 0
 			elseif self.lockedAxis == "Y" then
-				angle = Angle(0 , 0 , math.tau / 4)
+				delta.x = 0
+				delta.z = 0
 			elseif self.lockedAxis == "Z" then
-				angle = Angle(math.tau / 4 , 0 , 0)
+				delta.x = 0
+				delta.y = 0
 			end
 			
 			if self.isLocal then
-				angle = objectInfo.startTransform.angle * angle
+				delta = objectInfo.startTransform.angle * delta
 			end
 			
-			local transform = Transform3()
-			transform:Translate(pivot)
-			transform:Rotate(angle)
-			Render:SetTransform(transform)
-			
-			self.gizmoModel:Draw()
-			
-			Render:ResetTransform()
+			if self.gizmoModel then
+				local angle
+				if self.lockedAxis == "X" then
+					angle = Angle(0 , 0 , 0)
+				elseif self.lockedAxis == "Y" then
+					angle = Angle(0 , 0 , math.tau / 4)
+				elseif self.lockedAxis == "Z" then
+					angle = Angle(math.tau / 4 , 0 , 0)
+				end
+				
+				if self.isLocal then
+					angle = objectInfo.startTransform.angle * angle
+				end
+				
+				local transform = Transform3()
+				transform:Translate(pivot)
+				transform:Rotate(angle)
+				Render:SetTransform(transform)
+				
+				self.gizmoModel:Draw()
+				
+				Render:ResetTransform()
+			end
 		end
 	end
 	
