ec5bb9f4c7b86429c68e2e2835b55113bef33b43
dreadmullet <loosepolygon@gmail.com>
2014-06-21 16:16:33 +0000

    Breaking change to properties: the first data element is 0 or 1, which is whether the value is a table or not.

--- client/Core/cMEPropertyManager.lua
+++ client/Core/cMEPropertyManager.lua
@@ -46,19 +46,25 @@ function MapEditor.PropertyManager:Marshal()
 		-- Get value.
 		local value = nil
 		local isObject = false
-		-- Special handling for Objects - use their id instead.
+		-- Special handling for Objects - use their id instead. -1 means no Object.
 		if Objects[property.type] or Objects[property.subtype] then
 			isObject = true
 			
 			if property.type == "table" then
 				value = {}
-				for key , object in pairs(property.value) do
+				for index , object in ipairs(property.value) do
 					if object ~= MapEditor.Property.NoObject then
-						value[key] = object:GetId()
+						value[index] = object:GetId()
+					else
+						value[index] = -1
 					end
 				end
-			elseif property.value ~= MapEditor.Property.NoObject then
-				value = property.value:GetId()
+			else
+				if property.value ~= MapEditor.Property.NoObject then
+					value = property.value:GetId()
+				else
+					value = -1
+				end
 			end
 		else
 			value = MapEditor.Marshallable.MarshalValue(property.value)
@@ -75,8 +81,15 @@ function MapEditor.PropertyManager:Marshal()
 			actualType = property.type
 		end
 		
+		local tableVar
+		if property.type == "table" then
+			tableVar = 1
+		else
+			tableVar = 0
+		end
+		
 		-- Assign the property.
-		t.properties[name] = {FNV(actualType) , value}
+		t.properties[name] = {tableVar , FNV(actualType) , value}
 	end
 	
 	return t
@@ -86,19 +99,55 @@ function MapEditor.PropertyManager:Unmarshal(properties)
 	for name , propertyData in pairs(properties) do
 		local property = self:GetProperty(name)
 		if property then
-			local typeHash = propertyData[1]
-			local value = propertyData[2]
+			local isTable = propertyData[1] == 1
+			local typeHash = propertyData[2]
+			local value = propertyData[3]
 			
-			if property.type == "table" then
+			if isTable then
 				property.value = {}
-				for key , value in pairs(value) do
-					property.value[key] = MapEditor.Marshallable.UnmarshalValue(typeHash , value)
+				for index , value in ipairs(value) do
+					property.value[index] = MapEditor.PropertyManager.UnmarshalValue(typeHash , value)
 				end
 			else
-				property.value = MapEditor.Marshallable.UnmarshalValue(typeHash , value)
+				property.value = MapEditor.PropertyManager.UnmarshalValue(typeHash , value)
 			end
 		else
 			warn("Property doesn't exist: "..tostring(name))
 		end
+		
+		if self.OnPropertyChange then
+			self:OnPropertyChange{
+				name = property.name ,
+				newValue = property.value ,
+			}
+		end
 	end
+end
+
+MapEditor.PropertyManager.UnmarshalValue = function(typeHash , value)
+	local unmarshalFunction = MapEditor.PropertyManager.UnmarshalByTypeHash[typeHash]
+	if unmarshalFunction then
+		return unmarshalFunction(value)
+	else
+		return value
+	end
+end
+
+MapEditor.PropertyManager.UnmarshalByTypeHash = {}
+
+MapEditor.PropertyManager.UnmarshalByTypeHash[FNV("Object")] = function(value)
+	if value == -1 then
+		return MapEditor.Property.NoObject
+	else
+		local object = MapEditor.map:GetObject(value)
+		if object == nil then
+			error("Couldn't find object with id: "..tostring(value))
+		else
+			return object
+		end
+	end
+end
+
+MapEditor.PropertyManager.UnmarshalByTypeHash[FNV("Color")] = function(value)
+	return Color(value[1] , value[2] , value[3] , value[4])
 end
